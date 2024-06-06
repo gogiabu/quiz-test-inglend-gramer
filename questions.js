1d7c63294f922efd1836b2a35dcd1afea748ab4f
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-06-06 14:45:40 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,185 @@
+let questions = [
+    {
+        numb: 1,
+        question: "_____ to a rock concert?",
+        answer: "B. Have you ever been",
+        options: [
+            "A. Were you ever go ",
+            "B. Have you ever been",
+            "C. Do you ever go ",
+            "D. Have you ever go"
+        ]
+    },
+    {
+        numb: 2,
+        question: " I _____ the champion last week.",
+        answer: "A.  saw",
+        options: [
+            "A.  saw",
+            "B. have seen",
+            "C.  see",
+            "D. seen",
+        ]
+    },
+    {
+        numb: 3,
+        question: "I love rock and roll. I _____ it all my life.",
+        answer: "D. have liked",
+        options: [
+            "A. am liking",
+            "B. like",
+            "C. liking",
+            "D. have liked",
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "The Flash’s concert _____ fantastic 3 years ago.",
+        answer: "A. was",
+        options: [
+            "A. was",
+            "B. has",
+            "C. have been",
+            "D. are",
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "I _____ all their records since then",
+        answer: "D. bought",
+        options: [
+            "A. bought",
+            "B. buy",
+            "C. buyed",
+            "D. bought",
+
+        ]
+
+    },
+    {
+        numb: 6,
+        question: "The Flash _____ together for over fifteen years.",
+        answer: "B. have been",
+        options: [
+            "A. are",
+            "B. have been",
+            "C. are being",
+            "D. heve",
+
+        ]
+        
+    },
+    {
+        numb: 7,
+        question: "He’s my sister’s son. He’s my _____.",
+        answer: "B. nephew",
+        options: [
+            "A. uncle",
+            "B. nephew",
+            "C. niece",
+            "D. bride",
+
+        ]
+        
+    },
+    {
+        numb: 8,
+        question: " I run in races. I’m a(n) _____.",
+        answer: "C. athlete",
+        options: [
+            "A. pilot",
+            "B. teenager",
+            "C. athlete",
+            "D. chef",
+
+        ]
+        
+    },
+    {
+        numb: 9,
+        question: "In my job I wear the latest fashions. I’m a(n) _____.",
+        answer: "D. model",
+        options: [
+            "A. actor",
+            "B. professor",
+            "C. nurse",
+            "D. model",
+
+        ]
+        
+    },
+    {
+        numb: 10,
+        question: "I serve you drinks on a plane flight. I’m a _____.",
+        answer: "B. flight attendant",
+        options: [
+            "A. cousin",
+            "B. flight attendant",
+            "C. niece",
+            "D. child",
+
+        ]
+        
+    },
+   
+]
+// {
+//     numb: 1,
+//     question: "ამათგან რომელი ქვეყანა არ ესაზღვრება სლოვენიას?",
+//     answer: "D. სერბეთი",
+//     options: [
+//         "A. ხორვატია",
+//         "B. უნგრეთი",
+//         "C. იტალია",
+//         "D. სერბეთი"
+//     ]
+// },
+// {
+//     numb: 2,
+//     question: "რომელოი ქვეყანა ესაზღვრება ადრიატიკის ზღვას?",
+//     answer: "B. ხორვატია",
+//     options: [
+//         "A. საბერძნეთი",
+//         "B. ხორვატია",
+//         "C. საფრანგეთი",
+//         "D. პორტუგალია",
+//     ]
+// },
+// {
+//     numb: 3,
+//     question: "რამდენი ქვეყანა ესაზღვრება ესპანეთს?",
+//     answer: "C. 3",
+//     options: [
+//         "A. 2",
+//         "B. 4",
+//         "C. 3",
+//         "D. 5",
+
+//     ]
+// },
+// {
+//     numb: 4,
+//     question: "რომელ ქვეყანას ეკუთვნის კანარის კუნძულები?",
+//     answer: "A. ესპანეთს",
+//     options: [
+//         "A. ესპანეთს",
+//         "B. პორტუგალიას",
+//         "C. მაროკოს",
+//         "D. დიდ ბრიტანეთს",
+
+//     ]
+// },
+// {
+//     numb: 4,
+//     question: "ევროპის ყველაზე დიდი ტბა? არ შედის რუსეთის ტბები",
+//     answer: "B. ვენერნის ტბა",
+//     options: [
+//         "A. ბლედის ტბა",
+//         "B. ვენერნის ტბა",
+//         "C. კომოს ტბა",
+//         "D. ბოდენის ტბა",
+
+//     ]
+// },
